056137d5c52d5e2389fa16bc8a48cc688564fd44
artkpt <179370395+artkpt@users.noreply.github.com>
2026-08-11 13:18:30 +0700

    integrate api gateway

--- web-client/vite.config.ts
+++ web-client/vite.config.ts
@@ -10,15 +10,19 @@ export default defineConfig({
   ],
   server:{
     proxy:{
-      '/api/note':{
-        target: 'http://note-service:3333',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api\/note/, '')
-      },
-      '/api/auth':{
-        target: 'http://host.docker.internal:4000',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api\/auth/, '')
+      // '/api/note':{
+      //   target: 'http://note-service:3333',
+      //   changeOrigin: true,
+      //   rewrite: (path) => path.replace(/^\/api\/note/, '')
+      // },
+      // '/api/auth':{
+      //   target: 'http://host.docker.internal:4000',
+      //   changeOrigin: true,
+      //   rewrite: (path) => path.replace(/^\/api\/auth/, '')
+      // },
+      '/api':{
+        target: 'http://api-gateway:8080',
+        changeOrigin: true
       }
     },
     watch: {
