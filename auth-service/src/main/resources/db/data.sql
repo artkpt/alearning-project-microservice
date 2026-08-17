5477803e688412fa82673e4eb718be5c135ce4ee
artkpt <179370395+artkpt@users.noreply.github.com>
2026-08-17 19:03:33 +0700

    fix bug init db

--- auth-service/src/main/resources/db/data.sql
+++ auth-service/src/main/resources/db/data.sql
@@ -1,6 +1,6 @@
 INSERT INTO users(username, password, role)
-SELECT "admin", "admin", "admin"
-WHERE NOT EXISTS (SELECT 1 FROM users WHERE username = "admin");
+SELECT "admin2", "$argon2id$v=19$m=16384,t=2,p=1$pyp58kGXLJPsfkWXRixvXQ$C9UV4b9Q1RPHi16EMK7OXfzTp/h9XKxv5WI/ys4KseM", "admin"
+WHERE NOT EXISTS (SELECT 1 FROM users WHERE username = "admin2");
 
 
 INSERT INTO users(username, password, role)
