d218712e8616a098b08bc552489a5fd5d6467782
artkpt <179370395+artkpt@users.noreply.github.com>
2026-09-03 00:57:56 +0700

    setup shadcn&lucide react

--- web-client/vite.config.ts
+++ web-client/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,6 +9,11 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   server:{
     proxy:{
       // '/api/note':{
@@ -21,7 +27,7 @@ export default defineConfig({
       //   rewrite: (path) => path.replace(/^\/api\/auth/, '')
       // },
       '/api':{
-        target: 'http://host.docker.internal:8080',
+        target: 'http://localhost:8080',
         changeOrigin: true
       }
     },
