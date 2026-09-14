cc8ea68ab43d696afe0a5a723c8924f03208f10b
artkpt <179370395+artkpt@users.noreply.github.com>
2026-09-15 05:29:35 +0700

    add modelMapper dependency

--- course-service/src/main/java/com/example/alearning/courseservice/CourseServiceApplication.java
+++ course-service/src/main/java/com/example/alearning/courseservice/CourseServiceApplication.java
@@ -1,9 +1,12 @@
 package com.example.alearning.courseservice;
 
 import com.example.alearning.courseservice.configs.FileStorageProperties;
+import com.example.alearning.courseservice.utils.ListMapper;
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableConfigurationProperties({
@@ -13,6 +16,16 @@ public class CourseServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CourseServiceApplication.class, args);
+    }
+
+    @Bean
+    public ModelMapper modelMapper(){
+        return  new ModelMapper();
+    }
+
+    @Bean
+    public ListMapper listMapper(){
+        return ListMapper.getInstance();
     }
 
 }
